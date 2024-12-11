b0dd2c4a64e405473a729dde3ac35a3253612572
bloktools <prodbychime@gmail.com>
2024-12-10 23:43:42 -0500

    finish reactive layout

--- global/choose_seats_script.js
+++ global/choose_seats_script.js
@@ -21,20 +21,29 @@ $('.back').click(()=>{
 })
 
 
-function showSeatThingy(t, d, p, img) {
+function showSeatThingy(t, d, p, img, ter) {
     $('.seat-information h1').text(t);
     $('.seat-information p').text(d);
     $('.seat-information h3').text(p);
     $('.seat-information img').attr('src', img);
+    $('.seat-information button').css('background', (ter===1) ? 'var(--teritary)' : 'var(--primary)');
     $('body>*').hide();
     $('.seat-information').show();
     $('.back').show();
 }
 
-$('.container-top-section>*, .container-bottom-section>*').on('click', function () {
+$('.container-top-section>*').on('click', function () {
     const h1 = $(this).text();
     const loadedEvent = JSON.parse(localStorage.getItem('loadedEvent'));
 
-    showSeatThingy(h1, loadedEvent.title, loadedEvent.price, 'https://placehold.co/10x10')
+    showSeatThingy(h1, loadedEvent.title, loadedEvent.price, 'https://placehold.co/10x10', 0)
+    console.log(localStorage.getItem('loadedEvent'))
+})
+
+$('.container-bottom-section>*').on('click', function () {
+    const h1 = $(this).text();
+    const loadedEvent = JSON.parse(localStorage.getItem('loadedEvent'));
+
+    showSeatThingy(h1, loadedEvent.title, loadedEvent.price, 'https://placehold.co/10x10', 1)
     console.log(localStorage.getItem('loadedEvent'))
 })
--- info/seating/script.js
+++ info/seating/script.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const seats = document.querySelectorAll('.seat.available');
 
-    seats.forEach(seat => {
-        seat.addEventListener('click', () => {
-            if (!seat.classList.contains('occupied')) {
-                seat.classList.toggle('selected');
-            }
-        });
-    });
-});
+$(()=>{
+    localStorage.setItem('loadedEvent', ''+JSON.stringify({
+        'title': '',
+        'description': '',
+        'price': '',
+        'dates': '',
+        'time': ''
+    }))
+})
